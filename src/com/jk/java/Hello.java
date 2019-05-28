f67a6a321b3909496c5f12cf41b327a57b0c9865
金友库 <869536360@qq.com>
2019-05-28 10:27:46 +0800

    添加时间

--- src/com/jk/java/Hello.java
+++ src/com/jk/java/Hello.java
@@ -1,7 +1,19 @@
 package com.jk.java;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("sdfgh");
+
+        System.out.println(criatDate());
+    }
+
+    private static String criatDate() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = simpleDateFormat.format(new Date());
+
+        return format;
     }
 }
